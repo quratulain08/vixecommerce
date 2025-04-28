6e2422a8826e64c1eae8e150d3f894877f0f1bfe
quratulain08 <141954239+quratulain08@users.noreply.github.com>
2025-04-28 21:04:49 +0500

    Landing page update

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,6 +3,7 @@ import './LandingPage.css';
 import GetStarted from '../component/LandingPage/GetStarted/GetStarted';
 import WhatWeOffer from '../component/LandingPage/WhatWeOffer/WhatWeOffer';
 import WhyChooseUs from '../component/LandingPage/WhyChooseUs/WhyChooseUs';
+import Testimonials from '../component/LandingPage/Testimonial/Testimonial';
 
 export default function LandingPage() {
   return (
@@ -10,6 +11,7 @@ export default function LandingPage() {
       <GetStarted />
       <WhatWeOffer/>
       <WhyChooseUs/>
+      <Testimonials/>
     </div>
   );
 }
